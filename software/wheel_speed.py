e494da0a767abb51e46b6a2e7176891b3e0acd6b
amchagas <andremaia.chagas@gmail.com>
2020-02-19 13:47:50 +0000

    more work on python code

--- software/wheel_speed.py
+++ software/wheel_speed.py
@@ -1,5 +1,8 @@
 import pandas as pd
+#import seaborn as sns
+import matplotlib.pyplot as plt
 import os
+import numpy as np
 #filePath = "C:\Users\andre\Documents\GitHub\rodent-tracking\test_data\"
 
 filePath = os.getcwd()
@@ -9,9 +12,42 @@ oneDirUp = os.path.dirname(filePath)
 filePath = oneDirUp + "/test_data/"
 fileName = "tracking_wheel2020-02-10T10_22_19.csv"
 
+#open a csv file and store contents as a dataframe in fid
 fid = pd.read_csv(filePath+fileName)
 
-fid = fid[20:]
+
+#drop the first 20 entries. (temp since there was a camera adjustment in the start of this recording.)
+fid = fid.drop(list(range(0,21)))
+
+#find not NaN values
+detectIndex = fid['Value.X'].notna()
+
+#get time stamps of non nan values
+times = fid['Timestamp'][detectIndex][:]
+
+#transform the series into a datetime series, so that we can make mathematical operations on them
+times = pd.to_datetime(times)
+
+# subtract each timepoint N from N+1 (to get the time interval between each pass of the wheel through the video marker)
+times.diff()
+
+# subtract each index N from N+1. 
+# values =< 2 indicate the times the wheel marker was going through the threshold window, so we want to ignore those.
+wheelInd = np.diff(times.index)>1
+#add an False entry to the end so that this array can be used to index "times"
+wheelInd = np.append(wheelInd,False)
+
+#these are all the time stamps (the first of each detection burst) where the wheel marker was detected
+times[wheelInd]
+interval = list(times[wheelInd].diff())
+
+plt.hist(interval[1:])
+
+np.array()
+
+
+#get the unique values
+#times.loc[wheelInd] = np.unique(wheelInd)
 
 #find non nan values
 

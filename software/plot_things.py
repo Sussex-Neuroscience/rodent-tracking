f861798e22966f086c0406347024daab0df0b9e8
amchagas <andremaia.chagas@gmail.com>
2020-02-21 14:44:25 +0000

    first play with plotting data

--- software/plot_things.py
+++ software/plot_things.py
@@ -2,3 +2,30 @@ import os
 import pandas as pd
 import seaborn as sns
 import numpy as np
+
+
+
+filepath = "/mnt/d/repositories/rodent-tracking/test_data/all_data1.csv"
+
+fid = pd.read_csv(filepath)
+
+# print all the variables names
+print(fid.keys())
+
+# set some variables
+wheelMoving = fid["Item1.Item6.wheelmoving"]
+mouseMoving = fid["Item1.Item3.mousemoving"]
+timeInterval = fid['Item1.Item7.frameinterval']
+
+#get running wheel time
+whellTime = timeInterval[wheelMoving].sum()
+
+#get animal moving time
+mouseTime = timeInterval[mouseMoving].sum()
+
+sns.distplot(fid['Item2.Value.Item4.mousey'])
+sns.distplot(fid['Item2.Value.Item3.mousex'])
+
+df = pd.DataFrame([fid['Item2.Value.Item3.mousex'],fid['Item2.Value.Item4.mousey']])
+
+sns.jointplot(x='Item2.Value.Item3.mousex', y='Item2.Value.Item4.mousey', data=fid,kind="hex")

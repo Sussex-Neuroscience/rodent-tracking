6c1c1c9b14d44a7aac03be03a9ff1e2635f0fb7c
amchagas <andremaia.chagas@gmail.com>
2020-02-21 09:47:13 +0000

    reworked bonsai code to already put out analysed data

--- software/wheel_speed.py
+++ software/wheel_speed.py
@@ -1,15 +1,18 @@
 import pandas as pd
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-#filePath = "C:\Users\andre\Documents\GitHub\rodent-tracking\test_data\"
+
+
 
 filePath = os.getcwd()
 
+filePath = "/mnt/d/repositories/rodent-tracking/test_data/"
 
-oneDirUp = os.path.dirname(filePath)
-filePath = oneDirUp + "/test_data/"
+#oneDirUp = os.path.dirname(filePath)
+#filePath = oneDirUp + "/test_data/"
+#filePath = filePath + "/test_data/"
 fileName = "tracking_wheel2020-02-10T10_22_19.csv"
 
 #open a csv file and store contents as a dataframe in fid
@@ -24,12 +27,15 @@ detectIndex = fid['Value.X'].notna()
 
 #get time stamps of non nan values
 times = fid['Timestamp'][detectIndex][:]
-
+#times = fid['Timestamp'][:]
 #transform the series into a datetime series, so that we can make mathematical operations on them
 times = pd.to_datetime(times)
 
-# subtract each timepoint N from N+1 (to get the time interval between each pass of the wheel through the video marker)
-times.diff()
+
+## subtract each timepoint N from N-1 (to get the time interval between each pass of the wheel through the video marker)
+#times.diff()
+##since we subtracted a previous value from the current value, the first entry will be NaN, let's remove it
+#times = times[1:]
 
 # subtract each index N from N+1. 
 # values =< 2 indicate the times the wheel marker was going through the threshold window, so we want to ignore those.
@@ -40,7 +46,17 @@ wheelInd = np.append(wheelInd,False)
 #these are all the time stamps (the first of each detection burst) where the wheel marker was detected
 times[wheelInd]
 
-intervals = times[wheelInd]
+intervals = times[wheelInd].diff()
+#since we subtracted a previous value from the current value, the first entry will be NaN, let's remove it
+intervals = intervals[1:]
+
+#get values as integers and not "datetime" they are initially converted to ns
+intervals = intervals.values.astype(int)
+#now convert them to something sensible like sec
+#                     / micro /milli / sec
+intervals = intervals / 1000 / 1000 / 1000
+
+
 
 
 

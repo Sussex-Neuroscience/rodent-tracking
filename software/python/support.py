feb699f5c3bb27647a456e128fa040c97401d13d
amchagas <andremaia.chagas@gmail.com>
2020-05-02 19:26:32 +0100

    repo clean up and code update

--- software/python/support.py
+++ software/python/support.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 
-
-
-
 def rename_rows(pandaDF):
     """
     this function renames the columns in the data frame so that is easier
@@ -15,9 +12,18 @@ def rename_rows(pandaDF):
     
     for key in pandaDF.keys():
         secondDot = key.find(".",6)
-        fid = pandaDF.rename(columns = {key:key[secondDot+1:]})
+        pandaDF = pandaDF.rename(columns = {key:key[secondDot+1:]})
     
-    return fid
+    return pandaDF
 
-def load_n_join_DFs(fileList):
-    pass
+def load_n_join_DFs(csvFiles):
+    """
+    take a list with csv file names and load all of them in one panda dataframe 
+    """
+    allData = pd.DataFrame()
+    for item in csvFiles:
+        fid = pd.read_csv(item)
+        fid = rename_rows(fid)
+        allData = allData.append(fid)
+    
+    return allData
